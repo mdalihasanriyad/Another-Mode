350596e6985e73c6d14bccbf749c45c26d61223f
mdalihasanriyad <ahriyad.com@gmail.com>
2026-03-09 16:34:30 +0600

    added card

--- src/components/PricingCart.jsx
+++ src/components/PricingCart.jsx
@@ -1,16 +1,129 @@
-import React from 'react';
+import React from "react";
 
-const PricingCart = ({pricing}) => {
-    return (
-        <div className='border-2 border-gray-300 rounded-lg p-4 m-4'>
-            <img src={pricing.image} alt={pricing.name} />
-            <h2 className='text-3xl'>{pricing.name}</h2>
-            <p>{pricing.description}</p>
-            <p className='text-2xl'>{pricing.price}</p>
-            <p>{pricing.features}</p>
-            <p>{pricing.duration}</p>
+const PricingCart = ({ pricing }) => {
+    const { name,title, price, features } = pricing;
+  return (
+    <div className="border-2 border-gray-300 rounded-lg p-4 m-4">
+      <div className="card w-96 bg-base-100 shadow-sm">
+        <div className="card-body">
+          <span className="badge badge-xs badge-warning">{name}</span>
+          <div className="flex justify-between">
+            <h2 className="text-3xl font-bold">{title}</h2>
+            <span className="text-xl">{price}</span>
+          </div>
+          <ul className="mt-6 flex flex-col gap-2 text-xs">
+            <li>
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                className="size-4 me-2 inline-block text-success"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="2"
+                  d="M5 13l4 4L19 7"
+                />
+              </svg>
+              <span>High-resolution image generation</span>
+            </li>
+            <li>
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                className="size-4 me-2 inline-block text-success"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="2"
+                  d="M5 13l4 4L19 7"
+                />
+              </svg>
+              <span>Customizable style templates</span>
+            </li>
+            <li>
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                className="size-4 me-2 inline-block text-success"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="2"
+                  d="M5 13l4 4L19 7"
+                />
+              </svg>
+              <span>Batch processing capabilities</span>
+            </li>
+            <li>
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                className="size-4 me-2 inline-block text-success"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="2"
+                  d="M5 13l4 4L19 7"
+                />
+              </svg>
+              <span>AI-driven image enhancements</span>
+            </li>
+            <li className="opacity-50">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                className="size-4 me-2 inline-block text-base-content/50"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="2"
+                  d="M5 13l4 4L19 7"
+                />
+              </svg>
+              <span className="line-through">Seamless cloud integration</span>
+            </li>
+            <li className="opacity-50">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                className="size-4 me-2 inline-block text-base-content/50"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="2"
+                  d="M5 13l4 4L19 7"
+                />
+              </svg>
+              <span className="line-through">
+                Real-time collaboration tools
+              </span>
+            </li>
+          </ul>
+          <div className="mt-6">
+            <button className="btn btn-primary btn-block">Subscribe</button>
+          </div>
         </div>
-    );
+      </div>
+    </div>
+  );
 };
 
 export default PricingCart;
